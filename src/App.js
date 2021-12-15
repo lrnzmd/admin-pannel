4ee67bd6340b12d2a6afbf92caffdd0be791618b
Lorenzo Amodeo <lrnzmd@gmail.com>
2021-12-15 21:36:56 +0100

    user end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from 'react'
 import './app.css'
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import UserList from './pages/userList/UserList';
 
 
 function App() {
   return (
-    <div>
-        <Topbar/>
-        <div className="container">
-          <Sidebar/>
-          <Home/>
-        </div>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/users" element={<UserList/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
