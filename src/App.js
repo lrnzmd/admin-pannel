470be5fba16bba9d49c77e470cc030a8d4684eb8
Lorenzo Amodeo <lrnzmd@gmail.com>
2021-12-15 21:53:00 +0100

    user done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
-import UserList from './pages/userList/UserList';
+import UserList from './pages/userList/UserList'
+import User from './pages/user/User'
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />}/>
           <Route path="/users" element={<UserList/>}/>
+          <Route path="/user/:userId" element={<User/>}/>
+
         </Routes>
       </div>
     </Router>
